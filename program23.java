1ab9668e010932d7ed1731d98780f99b7d3ce49f
pravalikakula <ppravalika386@gmail.com>
2025-11-03 17:53:36 +0530

    Intial commit

---
+++ program23.java
@@ -0,0 +1,14 @@
+A=int(input("enter a number of A"));
+B=int(input("enter a number of B"));
+print("In addition is A+B",+);
+print("In subtract is A-B",-);
+print("In multiplication is A*B",*);
+print("In division is A/B",/);
+print("In modules is A%B",%);
+
+
+
+
+
+
+
